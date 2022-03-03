6c31db0655e8519ff345dde04fe33c0684d80233
Graham Preston <grahampreston@comcast.net>
2022-03-03 00:05:12 -0500

    Changes

--- HTMLTransformer.js
+++ HTMLTransformer.js
@@ -4,7 +4,7 @@ module.exports = new Transformer({
 	async transform({ asset, options }) {
 		const html = await asset.getCode();
 		const regexp = /<template[^>]*data-timp-src=["'](.*)["'][^>]*>(?:<\/template>)?/igm;
-
+		console.log("Test");
 		console.log(regexp.exec(html));
 
 
