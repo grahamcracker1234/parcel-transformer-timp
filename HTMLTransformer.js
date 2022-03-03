08bae3ed2c99cdafe05a5dd4dc1159f267830f09
Graham Preston <grahampreston@comcast.net>
2022-03-03 01:07:40 -0500

    Changes

--- HTMLTransformer.js
+++ HTMLTransformer.js
@@ -1,27 +1,24 @@
 const { Transformer } = require("@parcel/plugin");
+const { MemoryFS } = require("@parcel/fs");
+const { createWorkerFarm } = require("@parcel/core");
+
+const workerFarm = createWorkerFarm();
+const outputFS = new MemoryFS(workerFarm);
 
 module.exports = new Transformer({
 	async transform({ asset, options }) {
 		let html = await asset.getCode();
-		const fs = options.inputFS;
+		// const fs = options.inputFS;
 		const regexp = /<template[^>]*data-timp-src=["'](.*)["'][^>]*>(?:<\/template>)?/igm;
 
 		// Each match has 0: `template element` and 1: `data-timp-src`
 		let matches = html.matchAll(regexp);
 
 		matches = Array.from(matches).map(m => [m[0], m[1]]);
-		// console.log(matches);
-
-		// for (const match of matches) {
-		// 	console.log(match[0]);
-		// 	console.log(match[1]);
-		// }
-		
-		// matches = Array.from(matches).map(m => [m[0], m[1]]);
-		// console.log(matches);
+		console.log(matches);
 		for (const [textToReplace, filePath] of matches) {
 			console.log(textToReplace, filePath)
-			const text = await fs.readFile(filePath, "utf-8");
+			const text = await outputFS.readFile(filePath, "utf-8");
 			console.log(text);
 		}
 
