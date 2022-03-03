c2c5e911ca5c37a814bec9ce86df9e880ba38592
Graham Preston <grahampreston@comcast.net>
2022-03-03 01:20:41 -0500

    Changes

--- HTMLTransformer.js
+++ HTMLTransformer.js
@@ -8,18 +8,17 @@ const outputFS = new MemoryFS(workerFarm);
 module.exports = new Transformer({
 	async transform({ asset, options }) {
 		let html = await asset.getCode();
-		// const fs = options.inputFS;
+		const fs = options.inputFS;
 		const regexp = /<template[^>]*data-timp-src=["'](.*)["'][^>]*>(?:<\/template>)?/igm;
 
 		// Each match has 0: `template element` and 1: `data-timp-src`
 		let matches = html.matchAll(regexp);
 
 		matches = Array.from(matches).map(m => [m[0], m[1]]);
-		console.log(matches);
 		try {
 			for (const [textToReplace, filePath] of matches) {
 				console.log(textToReplace, filePath)
-				const text = await outputFS.readFile(filePath, "utf-8");
+				const text = await fs.readFile(filePath, "utf-8");
 				console.log(text);
 			}
 		} catch (error) {
