7e3d1d8b3dd100684af532dc830794a1758dd3f8
Graham Preston <grahampreston@comcast.net>
2022-03-03 16:13:46 -0500

    First Beta

--- HTMLTransformer.js
+++ HTMLTransformer.js
@@ -1,33 +1,27 @@
 const { Transformer } = require("@parcel/plugin");
-const { MemoryFS } = require("@parcel/fs");
-const { createWorkerFarm } = require("@parcel/core");
-
-const workerFarm = createWorkerFarm();
-const outputFS = new MemoryFS(workerFarm);
+const url = require("url");
 
 module.exports = new Transformer({
 	async transform({ asset, options, resolve }) {
+		// console.log(config);
 		let html = await asset.getCode();
-		const fs = options.inputFS;
-		const regexp = /<template[^>]*data-timp-src=["'](.*)["'][^>]*>(?:<\/template>)?/igm;
+		let matches = [];
+		const regexp = /<template[^>]*data-src=["'](.*)["'][^>]*>(?:<\/template>)?/igm;
 
-		// Each match has 0: `template element` and 1: `data-timp-src`
-		let matches = html.matchAll(regexp);
-
-		matches = Array.from(matches).map(m => [m[0], m[1]]);
-		try {
+		// Do while there are still matches. Used to support recursive templates within templates.
+		do {
+			// Each regexp match has 0: `template element` and 1: `data-timp-src`
+			matches = Array.from(html.matchAll(regexp)).map(m => [m[0], m[1]]);
 			for (const [textToReplace, filePath] of matches) {
-				console.log(textToReplace, filePath)
-				const resolvedPath = await resolve(options.projectRoot, filePath)
-				console.log(resolvedPath);
-				const text = await fs.readFile(resolvedPath, "utf-8");
-				console.log(text);
+				// Ideally, I would like to use `resolve` parameter, but it always returns error, so `url` module is used.
+				const resolvedPath = decodeURI(url.resolve(asset.filePath, filePath));
+				const text = await options.inputFS.readFile(resolvedPath, "utf-8");
+				html = html.replace(textToReplace, text);
 			}
-		} catch (error) {
-			console.log(error);
-		}
 
-		asset.setCode(html);
+			asset.setCode(html);
+		} while (matches.length > 0);
+
 		return [asset];
 	}
 });
